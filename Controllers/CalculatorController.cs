77dc02031ec7b253aea06d4f4a90a54d097d7453
rbhuiyan <rbhuiyan@BDRBHUIYAN03>
2024-07-07 12:36:51 +0600

    Add project files.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculatorModel model)
+        {
+            model.Result = Calculate(model.FirstNumber, model.SecondNumber, model.Operator);
+            return View(model);
+        }
+
+        private double Calculate(double firstNumber, double secondNumber, string operatorUsed)
+        {
+            return operatorUsed switch
+            {
+                "+" => firstNumber + secondNumber,
+                "-" => firstNumber - secondNumber,
+                "*" => firstNumber * secondNumber,
+                "/" => firstNumber / secondNumber,
+                _ => throw new InvalidOperationException("Invalid operator"),
+            };
+        }
+    }
+}
---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorApp.Models
+{
+    public class CalculatorModel
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public string Operator { get; set; }
+        public double Result { get; set; }
+    }
+}
